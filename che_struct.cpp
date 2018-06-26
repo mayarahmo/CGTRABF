f9d05cb9e19a7f072d7fdc24a9ab9f8528e29a95
FilipeMSoares <fhilipemorandi@hotmail.com>
2018-06-26 18:47:10 -0300

    Corrigingo falha de compilação (precisa corrigir mais)
    Tive também de mudar o código um pouco. Foi necessário criar alguns ponteiros.

--- che_struct.cpp
+++ che_struct.cpp
@@ -1,30 +1,52 @@
+#ifndef CHE_LIB
+#define CHE_LIB
 #include "LA3D.cpp"
 #include <set>
 #include <stack>
 
+struct half_edge_struct ;
+typedef half_edge_struct HalfEdge;
+
 typedef struct he_point {
     Ponto p;
-    HalfEdge edge;
+    HalfEdge* edge;
 } HE_Point;
 
-bool compareHE_Points(const HE_Point& a, const HE_Point& b){
-    return a.p.x < b.p.x || a.p.y < b.p.y || a.p.z < b.p.z;
-}
-
 typedef struct t_face {
-    HalfEdge own;
+    HalfEdge* own;
 } Face;
 
-typedef struct half_edge_struct {
+struct half_edge_struct {
     HE_Point sc;
-    half_edge_struct* next, twin;
+    HalfEdge* next, *twin;
     Face f;
-} HalfEdge;
+};
+
+bool compareHE_Points(const HE_Point& a, const HE_Point& b){
+    return a.p.x <= b.p.x || a.p.y <= b.p.y || a.p.z <= b.p.z;
+}
+
+HalfEdge makeHalfEdge(Ponto p, HalfEdge* next, HalfEdge* twin, HalfEdge* face){
+    HalfEdge edge;
+
+    HE_Point he_p;
+    he_p.p = p;
+    he_p.edge = &edge;
+
+    edge.sc = he_p;
+
+    edge.next = next;
+    edge.twin = twin;
+
+    edge.f.own = face;
+
+    return edge;
+}
 
 bool compareHalfEdges(const HalfEdge& a, const HalfEdge& b){
     if(equals(a.sc.p,b.sc.p)){
-        int cnta = ((a.next == null)? 1:0)+((a.twin == null)?1:0) ;
-        int cntb = ((b.next == null)? 1:0)+((b.twin == null)?1:0) ;
+        int cnta = ((a.next == NULL)? 1:0)+((a.twin == NULL)?1:0) ;
+        int cntb = ((b.next == NULL)? 1:0)+((b.twin == NULL)?1:0) ;
         if(cnta >= cntb) return true;
         else return false;
     }
@@ -32,7 +54,9 @@ bool compareHalfEdges(const HalfEdge& a, const HalfEdge& b){
 }
 
 bool compareFaces(const Face& a, const Face& b){
-    return compareHalfEdges(a.own,b.own);
+    if(a.own == NULL) return false;
+    if(b.own == NULL) return true;
+    return compareHalfEdges(*a.own,*b.own);
 }
 
 typedef std::set<HE_Point,compareHE_Points> v_points;
@@ -60,8 +84,9 @@ bool addSurface(HalfEdgeMesh mesh,HalfEdge he){
         he = stackhe.top();
         stackhe.pop();
         addHalfEdge(mesh,he);
-        if(he.*next == null) stackhe.push(he->next);
-        if(he.*twin == null) stackhe.push(he->twin);
+        if(he.next == NULL) stackhe.push(*he.next);
+        if(he.twin == NULL) stackhe.push(*he.twin);
     }
     return true;
 }
+#endif
