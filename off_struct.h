db75475a6ca03ce3a7b44d9069cb06cf95e22fc1
Gabrielmarques92 <gabrielmarques92@hotmail.com>
2018-06-26 22:48:09 -0300

    Add files via upload

---
+++ off_struct.h
@@ -0,0 +1,29 @@
+#include "LA3D.cpp"
+#include <stdio.h>
+#include<vector>
+
+typedef struct t_off_set {
+    std::vector<Ponto> pontos;
+    std::vector<Triangulo> faces;
+} off_set;
+
+void insertPoint(off_set& set, Ponto p){
+	
+	set.pontos.push_back(p);
+	
+}
+
+void insertFace(off_set& set, int idx1, int idx2, int idx3){
+	
+	Triangulo t;
+
+	t.raiz = set.pontos[idx1];
+	t.direito = set.pontos[idx2];
+	t.esquerdo = set.pontos[idx3];
+
+	set.faces.push_back(t);
+	
+}
+
+
+
