4e3d4497ad158ae1926e4fd58121de4b491729aa
FilipeMSoares <fhilipemorandi@hotmail.com>
2018-06-28 23:52:45 -0300

    che_struct v4 (versão final)
    Funções testadas e funcionando! Atenção! Deve se ter cuidado ao indicar na half edge qual é a next, pois pode-se criar um loop infinito. Para saber se está tudo certo, basta verificar o ponto de next e twin em uma half edge. Eles devem ser os mesmos.

--- che_struct.cpp
+++ che_struct.cpp
@@ -2,7 +2,8 @@
 #define CHE_LIB
 #include "LA3D.cpp"
 #include <set>
-#include <stack>
+#include <queue>
+#include <cstdlib>
 
 struct half_edge_struct ;
 typedef half_edge_struct HalfEdge;
@@ -21,11 +22,16 @@ struct half_edge_struct {
 };
 
 bool compareHE_Points(const HE_Point& a, const HE_Point& b){
-    return a.p.x <= b.p.x || a.p.y <= b.p.y || a.p.z <= b.p.z;
+    if(a.p.x == b.p.x){
+        if(a.p.y == b.p.y) return a.p.z < b.p.z;
+        return a.p.y < b.p.y;
+    }
+    return a.p.x < b.p.x;
+    //return a.p.x < b.p.x || a.p.y < b.p.y || a.p.z < b.p.z;
 }
 
 HalfEdge makeHalfEdge(Ponto p, HalfEdge* next, HalfEdge* twin, Face* face){
-    HalfEdge edge;
+    HalfEdge edge;// = (HalfEdge*) malloc(sizeof(HalfEdge));
 
     HE_Point he_p;
     he_p.p = p;
@@ -42,12 +48,18 @@ HalfEdge makeHalfEdge(Ponto p, HalfEdge* next, HalfEdge* twin, Face* face){
 }
 
 struct CompareHE {
+    Ponto p; HalfEdge origem; int originCreated=0;
     inline bool operator()(const HalfEdge& a, const HalfEdge& b){
+        if(!originCreated){
+            p.x = 0; p.y = 0; p.z = 0;
+            origem = makeHalfEdge(p,NULL,NULL,NULL);
+            originCreated = 1;
+        }
         if(equals(a.sc.p,b.sc.p)){
-            int cnta = ((a.next == NULL)? 1:0)+((a.twin == NULL)?1:0) ;
-            int cntb = ((b.next == NULL)? 1:0)+((b.twin == NULL)?1:0) ;
-            if(cnta >= cntb) return true;
-            else return false;
+            HalfEdge c,d;
+            d = (b.next == NULL)? origem:*b.next;
+            c = (a.next == NULL)? origem:*a.next;
+            return compareHE_Points(c.sc,d.sc);
         }
         return compareHE_Points(a.sc,b.sc);
     }
@@ -61,7 +73,7 @@ bool compareFaces(const Face& a, const Face& b){
 //typedef std::set<HE_Point> v_points;
 //typedef std::set<Face> v_faces;
 typedef std::set<HalfEdge,CompareHE> v_h_edges;
-typedef std::stack<HalfEdge> h_e_stack;
+typedef std::queue<HalfEdge> h_e_stack;
 
 typedef struct t_half_edge_mesh {
     //v_points points(compareHE_Points);
@@ -76,15 +88,25 @@ bool addHalfEdge(HalfEdgeMesh& mesh, HalfEdge he){
     return true;
 }
 
-bool addSurface(HalfEdgeMesh& mesh,HalfEdge he){
+bool addSurface(HalfEdgeMesh& mesh,HalfEdge start){
     h_e_stack stackhe;
-    stackhe.push(he);
+    stackhe.push(start);
+    HalfEdge he;
+    int count = 0;
     while(!stackhe.empty()){
-        he = stackhe.top();
+        he = stackhe.front();
         stackhe.pop();
+        printf("%d %f %f %f ",mesh.h_edges.size(),he.sc.p.x,he.sc.p.y,he.sc.p.z);
+        printf("%d %d\n",he.next!=NULL,he.twin!=NULL);
+        if(he.next != NULL){
+            printf("%f %f %f\n",(*he.next).sc.p.x,(*he.next).sc.p.y,(*he.next).sc.p.z);
+        }
+        if(he.twin != NULL){
+            printf("%f %f %f\n",(*he.twin).sc.p.x,(*he.twin).sc.p.y,(*he.twin).sc.p.z);
+        }
         addHalfEdge(mesh,he);
-        if(he.next != NULL) stackhe.push(*he.next);
-        if(he.twin != NULL) stackhe.push(*he.twin);
+        if(he.next != NULL) if(mesh.h_edges.find(*he.next) == mesh.h_edges.end() ) stackhe.push(*he.next);
+        if(he.twin != NULL) if(mesh.h_edges.find(*he.twin) == mesh.h_edges.end() ) stackhe.push(*he.twin);
     }
     return true;
 }
